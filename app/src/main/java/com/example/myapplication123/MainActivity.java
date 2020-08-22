c41138051504789ecbfd48e97f18e40f0177a4eb
옥인성 <ois0531@naver.com>
2020-08-23 02:52:40 +0900

    main 수정 08/17 02:05 1) 각 방법 누를시 각각 화면으로 이동하는거 구현해야함. 2) 내정보 이름,성별,생년월일 인텐트로 받아와야함. - > 에러발생 3) 만약 처음일시 user_input으로 넘어가게 해야함. - > 구현
    - > 첫시작시 user_input넘어가는거 구현
    - > 내정보 이름 성별 생년월일 받아오는 코드를 짰지만 받아와지지않음..

--- app/src/main/java/com/example/myapplication123/MainActivity.java
+++ app/src/main/java/com/example/myapplication123/MainActivity.java
@@ -18,12 +18,10 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 private TextView textView1;
 
-    TextView background2;
-    TextView background3;
-    TextView background4;
-    TextView background5;
-    SharedPreferences first_action;
-
+    TextView background2,background3,background4,background5,
+            information_username,information_year,information_month,information_day;
+    SharedPreferences first_action,user_name,user_year,user_month,user_day;
+    Button test_btn;
 
     private static final String TAG = "MainActivity";
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +33,23 @@ private TextView textView1;
         background4 = findViewById(R.id.background_4);
         background5 = findViewById(R.id.background_5);
         first_action = getSharedPreferences("first_action",MODE_PRIVATE);
+        information_username =findViewById(R.id.username);
+        information_year = findViewById(R.id.main_year);
+        information_month = findViewById(R.id.main_month);
+        information_day = findViewById(R.id.main_day);
 
+        user_name = getSharedPreferences("user_name",MODE_PRIVATE);
+        user_year = getSharedPreferences("user_year",MODE_PRIVATE);
+        user_month = getSharedPreferences("user_month",MODE_PRIVATE);
+        user_day = getSharedPreferences("user_day",MODE_PRIVATE);
+        String name = user_name.getString("user_name","ERROR");
+        String year = user_name.getString("user_year","ERROR");
+        String month = user_name.getString("user_month","ERROR");
+        String day = user_name.getString("user_day","ERROR");
+        information_username.setText(name);
+        information_year.setText(year + ".");
+        information_month.setText(month + ".");
+        information_day.setText(day);//정보가 받아와지지않음. 해결해야함
         //첫실행체크코드
         boolean first_value = first_action.getBoolean("first_value",true);
         if(first_value){
@@ -44,6 +58,15 @@ private TextView textView1;
             startActivity(go_user_input);
             first_action.edit().putBoolean("first_value",false).apply();
         }
+        // test
+        test_btn = findViewById(R.id.test);
+        test_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent test = new Intent(getApplicationContext(),User_Input.class);
+                startActivity(test);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/myapplication123/User_Input.java
+++ app/src/main/java/com/example/myapplication123/User_Input.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class User_Input extends AppCompatActivity {
@@ -15,21 +18,22 @@ public class User_Input extends AppCompatActivity {
 
 
     //옥인성
-    Button main_activation_nextscreen_btn, main_datepicker_btn;
+    Button main_activation_nextscreen_btn,main_not_activation_nextscreen_btn,main_datepicker_btn;
     TextView main_text_year;
     TextView main_text_month;
     TextView main_text_day;
     EditText main_edit_name;
     TextView main_text_title;
     TextView main_datepicker_move;
-
     String year, month, day;
     int run = 0; // 생년월일 정보 입력여부
     //옥인성
 
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user__input);
 
 
         //옥인성
@@ -40,15 +44,14 @@ public class User_Input extends AppCompatActivity {
         main_edit_name = findViewById(R.id.main_edit_name);
         main_text_title = findViewById(R.id.main_text_title);
         main_datepicker_move = findViewById(R.id.datepicker_move);
-
-
+        main_activation_nextscreen_btn = findViewById(R.id.main_activation_nextscreen_btn);
+        main_not_activation_nextscreen_btn = findViewById(R.id.main_not_activation_nextscreen_btn);
         Intent intent = getIntent(); // DatePicker -> MainActivity 로 전달된 값을 받는다.
         if (intent.getStringExtra("year") != null) { // DatePicker -> MainActivity 전달된 값이 0이 아닐 경우 변수에 저장한다.
 
             year = intent.getStringExtra("year");
             month = intent.getStringExtra("month");
             day = intent.getStringExtra("day");
-
         }
 
         run = intent.getIntExtra("run", 0);
@@ -67,16 +70,25 @@ public class User_Input extends AppCompatActivity {
 
         // 년 월 일 코드
         //화면이동코드
-        main_activation_nextscreen_btn = findViewById(R.id.main_activation_nextscreen_btn);
         main_activation_nextscreen_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
-            }
-        }); // print이동
+                SharedPreferences year_save = getSharedPreferences("user_year",MODE_PRIVATE);
+                SharedPreferences month_save = getSharedPreferences("user_month",MODE_PRIVATE);
+                SharedPreferences day_save = getSharedPreferences("user_day",MODE_PRIVATE);
+                SharedPreferences.Editor editor2 = year_save.edit();
+                SharedPreferences.Editor editor3 = month_save.edit();
+                SharedPreferences.Editor editor4 = day_save.edit();
+                editor2.commit();
+                editor3.commit();
+                editor4.commit();
 
-        main_datepicker_move.setOnClickListener(new View.OnClickListener() {//"다음으로" 버튼 누르면 다음화면으로 이동하는거
+            }
+        }); // MainActivity 이동 &&생년월일 전달
+
+        main_datepicker_move.setOnClickListener(new View.OnClickListener() {// DatePicker로 이동
             @Override
             public void onClick(View v) {
                 Intent nextscreen = new Intent(getApplicationContext(), DatePicker.class);
@@ -85,12 +97,36 @@ public class User_Input extends AppCompatActivity {
         });//datepicker이동
         //화면이동코드
 
+
+
         // 이름저장코드
-        String text = main_edit_name.getText().toString(); // text에 edittext내용을 저장
+        String text = main_edit_name.getText().toString(); // text에 edit_text내용을 저장
         SharedPreferences name_save = getSharedPreferences("user_name", MODE_PRIVATE);
         SharedPreferences.Editor editor = name_save.edit();
         String name_data = name_save.getString("text", "");
         editor.commit();
         // 이름저장코드
+
+        main_edit_name.addTextChangedListener(new TextWatcher() { // edit_text가 입력이 끝나야 버튼활성화시키는 코드
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (run != 0) { // Datepicker 에서 생년월일까지 입력해야 버튼을 활성화 시킨다.
+                    main_not_activation_nextscreen_btn.setVisibility(View.GONE);
+                    main_activation_nextscreen_btn.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
     }
 }
